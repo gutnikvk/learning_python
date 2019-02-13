340c61766e68c8cd9af9599eafd19b6cb6979ffe
Владислав Гутник <gutnikvg@gmail.com>
2019-02-13 17:07:57 +0300

    Fibonacci better variant

---
+++ gvk/fibonacci/better_alg.py
@@ -0,0 +1,15 @@
+def check_input_number(n):
+    if n < 0: raise ValueError('It has to be >= 0')
+
+
+def get_fibonacci_number(n):
+    fibonacciRow = []
+    for i in range(n+1):
+        if i<=1: fibonacciRow.append(i)
+        else: fibonacciRow.append(fibonacciRow[i-1] + fibonacciRow[i-2])
+    return fibonacciRow[n]
+
+if __name__ == '__main__':
+    n = int(input('Input a positive integer\n'))
+    check_input_number(n)
+    print(get_fibonacci_number(n))
