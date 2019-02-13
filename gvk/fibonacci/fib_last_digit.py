7c209bac3bcad30b996df5e3ec46f9d68ea088b8
Владислав Гутник <gutnikvg@gmail.com>
2019-02-13 17:19:52 +0300

    Fibonacci last digit

--- gvk/fibonacci/fib_last_digit.py
+++ gvk/fibonacci/fib_last_digit.py
@@ -2,15 +2,15 @@ def check_input_number(n):
     if n < 0: raise ValueError('It has to be >= 0')
 
 
-def get_fibonacci_number(n):
+def get_fibonacci_number_last_digit(n):
     fibonacciRow = []
     for i in range(n+1):
         if i<=1: fibonacciRow.append(i)
-        else: fibonacciRow.append(fibonacciRow[i-1] + fibonacciRow[i-2])
+        else: fibonacciRow.append((fibonacciRow[i-1] + fibonacciRow[i-2]) % 10)
     return fibonacciRow[n]
 
 
 if __name__ == '__main__':
     n = int(input('Input a positive integer\n'))
     check_input_number(n)
-    print(get_fibonacci_number(n))
+    print(get_fibonacci_number_last_digit(n))
