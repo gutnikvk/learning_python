bf3f4204d1bb2a12386589ff5af56445e4973cae
Владислав Гутник <gutnikvg@gmail.com>
2019-02-14 11:21:57 +0300

    Exercises

--- gvk/greatest_common_divisor/naive.py
+++ gvk/greatest_common_divisor/naive.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     a, b = map(int, input('Input two integers').split())
     gcd = 1
-    for d in range(1, max(a, b) + 1):
+    for d in range(1, min(a, b) + 1):
         if a % d == 0 and b % d == 0.0: gcd = d
     print(gcd)
     input()
